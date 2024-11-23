449680886da05382a54b5a2b0c574371ba621304
Marianne Sruthi <156935047+mariannesruthi45@users.noreply.github.com>
2024-11-23 10:48:41 +0530

    Add files via upload

---
+++ SwapStrings.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class SwapStrings {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first string:");
+        String str1 = scanner.nextLine();
+        System.out.println("Enter the second string:");
+        String str2 = scanner.nextLine();
+        str1 = str1 + str2;
+        str2 = str1.substring(0, str1.length() - str2.length());
+        str1 = str1.substring(str2.length());
+        System.out.println("After swapping:");
+        System.out.println("First string: " + str1);
+        System.out.println("Second string: " + str2);
+    }
+}
